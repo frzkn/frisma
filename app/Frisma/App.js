2a83de72007d079ed9b429cfd68147b8528cb66c
Faraz Khan <farazrk001@gmail.com>
2019-06-05 22:31:33 +0530

    Remove Swiper Page

--- app/Frisma/App.js
+++ app/Frisma/App.js
@@ -2,9 +2,9 @@ import React from "react"
 import { StyleSheet, Text, View } from "react-native"
 import { Header, Content, Container } from "native-base"
 import Swiper from "react-native-swiper"
-import {CameraComponent as Camera} from './Components/Camera'
-import {FiltersComponent as Filters} from './Components/Filters'
-import {GalleryComponent as Gallery} from './Components/Gallery'
+import { CameraComponent as Camera } from "./Components/Camera"
+import { FiltersComponent as Filters } from "./Components/Filters"
+import { GalleryComponent as Gallery } from "./Components/Gallery"
 
 const styles = StyleSheet.create({
   container: {
@@ -24,13 +24,10 @@ export default class App extends React.Component {
   render() {
     return (
       <Container>
-        <Swiper index={0} loop={false} showsPagination={false}>
+        <Swiper index={1} loop={false} showsPagination={false}>
           <Filters />
           <Camera />
           <Gallery/>
-          <View style={styles.container}>
-            <Text style={styles.content}>Hello world</Text>
-          </View>
         </Swiper>
       </Container>
     )
